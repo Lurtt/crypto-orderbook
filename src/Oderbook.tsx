139b46d3f1eed3b90546e9233a0bbc07894df378
lurtt <lurttik@gmail.com>
2022-01-22 23:07:25 +0100

    feat: GH-6 WIP update <Orderbook /> UI

--- src/Oderbook.tsx
+++ src/Oderbook.tsx
@@ -1,33 +1,53 @@
-import { FC, useCallback } from 'react';
+import { FC, useCallback, useMemo } from 'react';
 
 import { useOrderbook } from './useOrderbook'
 
 interface Props {
   data: Order
+  maxTotal: number
 }
 
-const Row: FC<Props> = ({ data }) => {
-  const [price, size] = data
+const Row: FC<Props> = ({ data, maxTotal }) => {
+  const [price, size, total] = data
 
-  return <div>{price}, {size}</div>
+  const getPercentageWidth = useMemo(() => {
+    const value = ((total / (maxTotal ?? 1)) * 100)
+
+    return `${value}%`
+  }, [total, maxTotal])
+
+  return (
+    <div style={{ position: 'relative', height: 28 }}>
+      <div style={{ background: 'transparent' }}>PRICE: {price}, SIZE: {size}, TOTAL: {total}</div>
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        width: getPercentageWidth,
+        height: '100%',
+        backgroundColor: '#15d41566'
+      }}></div>
+    </div>
+  )
 }
 
 export const Oderbook: FC = () => {
-  const state = useOrderbook({ maxRecords: 25 })
+  const { bids, asks, maxTotal } = useOrderbook({ maxRecords: 20 })
 
-  const renderRow = useCallback((order) => <Row key={order[0]} data={order} />, [])
+  const renderRow = useCallback((order: Order) => <Row key={order[0]} data={order} maxTotal={maxTotal} />, [maxTotal])
 
   return (
     <>
+      <h1>MAX TOTAL:{maxTotal}</h1>
       <div style={{ display: 'flex' }}>
         <div style={{ display: 'flex', flex: 1, flexDirection: 'column' }}>
           <h1>BIDS/BUY</h1>
-          {state.bids.map(renderRow)}
+          {bids.map(renderRow)}
         </div>
 
         <div style={{ display: 'flex', flex: 1, flexDirection: 'column' }}>
           <h1>ASKS/SELL</h1>
-          {state.asks.map(renderRow)}
+          {asks.map(renderRow)}
         </div>
       </div>
     </>
