afa0293367f25529fab222e0e3888f4a4359ab88
Lukas Pospisil <Lurtt@users.noreply.github.com>
2022-01-24 16:05:31 +0100

    feat: GH-8 Init tailwind
    * feat: GH-8 Add tailwind lib
    
    * feat: GH-8 Init tailwind
    
    * feat: GH-8 Configure tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css'
+
 import App from './App';
 import { OrdersProvider } from './OrdersContext';
 import reportWebVitals from './reportWebVitals';
