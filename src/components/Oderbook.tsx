0d1a1fd9b6c29dc9061f40ffc2d0b78a32c5dac7
Lukas Pospisil <Lurtt@users.noreply.github.com>
2022-02-01 22:05:40 +0100

    feat: GH-17 Toggle markets
    * feat: GH-17 Define ETH constant
    
    * feat: GH-17 Create toggle markets reducer
    
    * feat: GH-17 Provide sendJsonMessage from useOrderbook
    
    * feat: GH-17 Display button with toggle market dispatcher
    
    * feat: GH-17 Format files

--- src/components/Oderbook.tsx
+++ src/components/Oderbook.tsx
@@ -2,23 +2,28 @@ import { FC, useCallback } from 'react';
 
 import { useOrderbook } from 'hooks/useOrderbook';
 import { Indicator } from './Indicator';
+import { useIndicators } from 'contexts/indicators-context/IndicatorsContext';
 
 export const Oderbook: FC = () => {
-  const { bids, asks, maxTotal } = useOrderbook({ maxRecords: 20 });
+  const { bids, asks, maxTotal, sendJsonMessage } = useOrderbook({ maxRecords: 20 });
+  const { dispatch } = useIndicators();
 
   const renderRow = useCallback((type: IndicatorType) => (indicator: Indicator) => <Indicator key={indicator[0]} type={type} data={indicator} maxTotal={maxTotal} />, [maxTotal]);
 
   return (
-    <div className="flex">
-      <div className="flex flex-1 flex-col">
-        <h1>BIDS/BUY</h1>
-        {bids.map(renderRow('buy'))}
-      </div>
+    <>
+      <button onClick={() => dispatch({ type: 'TOGGLE_MARKETS', payload: { sendJsonMessage } })}>TOGGLE MARKET</button>
+      <div className="flex">
+        <div className="flex flex-1 flex-col">
+          <h1>BIDS/BUY</h1>
+          {bids.map(renderRow('buy'))}
+        </div>
 
-      <div className="flex flex-1 flex-col">
-        <h1>ASKS/SELL</h1>
-        {asks.map(renderRow('sell'))}
+        <div className="flex flex-1 flex-col">
+          <h1>ASKS/SELL</h1>
+          {asks.map(renderRow('sell'))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
