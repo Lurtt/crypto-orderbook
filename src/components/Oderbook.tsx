d8e0a33dbed446998a9634e48241f7341c2ca16a
lurtt <lurttik@gmail.com>
2022-02-01 22:02:42 +0100

    feat: GH-17 Display button with toggle market dispatcher

--- src/components/Oderbook.tsx
+++ src/components/Oderbook.tsx
@@ -2,13 +2,18 @@ import { FC, useCallback } from 'react';
 
 import { useOrderbook } from 'hooks/useOrderbook';
 import { Indicator } from './Indicator';
+import { useIndicators } from 'contexts/indicators-context/IndicatorsContext';
 
 export const Oderbook: FC = () => {
-  const { bids, asks, maxTotal } = useOrderbook({ maxRecords: 20 });
+  const { bids, asks, maxTotal, sendJsonMessage } = useOrderbook({ maxRecords: 20 });
+  const { dispatch } = useIndicators();
 
   const renderRow = useCallback((type: IndicatorType) => (indicator: Indicator) => <Indicator key={indicator[0]} type={type} data={indicator} maxTotal={maxTotal} />, [maxTotal]);
 
+  
   return (
+    <>
+    <button onClick={() => dispatch({ type: 'TOGGLE_MARKETS', payload: { sendJsonMessage } })}>TOGGLE MARKET</button>
     <div className="flex">
       <div className="flex flex-1 flex-col">
         <h1>BIDS/BUY</h1>
@@ -20,5 +25,6 @@ export const Oderbook: FC = () => {
         {asks.map(renderRow('sell'))}
       </div>
     </div>
+    </>
   );
 };
