f5b9134902a36e6b48e644837e0c51a0d922eab7
lurtt <lurttik@gmail.com>
2022-02-02 21:35:57 +0100

    feat: GH-19 Update layout component design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 export const Layout: FC = ({ children }) => (
-  <main className="py-6 bg-gray-900 text-gray-100">
+  <main className="py-6 px-8 bg-gray-900 text-gray-100">
     {children}
   </main>
 );
