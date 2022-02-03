effdfcd7a244659443770e2d72e73789511a8c42
lurtt <lurttik@gmail.com>
2022-02-03 13:29:36 +0100

    feat: GH-19 Create <ToggleMarket /> button component

---
+++ src/components/ToggleMarket.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+
+interface Props {
+  onClick: () => void
+}
+
+export const ToggleMarket: FC<Props> = ({ onClick }) => (
+  <button className='block mx-auto px-5 py-2 rounded-md bg-purple-600' onClick={onClick}>Toggle Feed</button>
+);
+
